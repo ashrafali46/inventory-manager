fe60208d7e0770b1f9ba18c945d99e6e4b53e35d
johnmcraig <jmcraig32884@gmail.com>
2020-04-23 23:54:02 -0400

    Created Address model for customer relationship

---
+++ src/Data/Models/CustomerAddress.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data.Models
+{
+    public class CustomerAddress
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+
+        [MaxLength(100)]
+        public string AddressLine1 { get; set; }
+
+        [MaxLength(100)]
+        public string AddressLine2 { get; set; }
+
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        
+        
+    }
+}
