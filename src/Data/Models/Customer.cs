81993a7b45fa2f93f80df21f6a8fd02cbb881b5c
johnmcraig <jmcraig32884@gmail.com>
2020-04-26 09:51:29 -0400

    Added FullName property

--- src/Data/Models/Customer.cs
+++ src/Data/Models/Customer.cs
@@ -7,6 +7,7 @@ namespace Data.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
         public DateTime CreatedOn { get; set; }
         public DateTime UpdatedOn { get; set; }
 
