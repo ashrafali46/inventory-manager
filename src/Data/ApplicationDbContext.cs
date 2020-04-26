f3ed102889c2a75476692ffe88dd10876c1102ab
johnmcraig <jmcraig32884@gmail.com>
2020-04-26 09:35:44 -0400

    Added configuration and options to implement database connection in context class

--- src/Data/ApplicationDbContext.cs
+++ src/Data/ApplicationDbContext.cs
@@ -1,20 +1,34 @@
 using Data.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-     public ApplicationDbContext() {}
+        private readonly IConfiguration _config;
+        public ApplicationDbContext()
+        { }
 
-     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-     {
-         
-     }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration config) : base(options)
+        {
+            _config = config;
+        }
 
-     public virtual DbSet<Customer> Customers { get; set; } 
-     public virtual DbSet<Product> Products { get; set; } 
-     public virtual DbSet<CustomerAddress> CustomerAddress { get; set; } 
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<CustomerAddress> CustomerAddress { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.EnableDetailedErrors();
+            optionsBuilder.UseNpgsql(_config.GetConnectionString("placeholder"));
+        }
     }
 }
