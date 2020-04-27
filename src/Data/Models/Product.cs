e4ac452b1df9aef34dabf41ca27edc7b1210cf72
johnmcraig <jmcraig32884@gmail.com>
2020-04-26 22:45:07 -0400

    Added additional properties to Product class

--- src/Data/Models/Product.cs
+++ src/Data/Models/Product.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace Data.Models
@@ -6,5 +7,15 @@ namespace Data.Models
     {
         [Key]
         public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        
+        [MaxLength(64)]
+        public string Name { get; set; }
+        [MaxLength(128)]
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsTaxable { get; set; }
+        public bool IsArchived { get; set; }
     }
 }
