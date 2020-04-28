7a728c5fbb96b70e6b7a06c0df71631ca500ef8f
johnmcraig <jmcraig32884@gmail.com>
2020-04-28 00:14:16 -0400

    Intoduced ProductService field, began to further implement controller actions

--- src/Api/Controllers/ProductController.cs
+++ src/Api/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Services.Product;
 
 namespace Api.Controllers
 {
@@ -7,16 +8,30 @@ namespace Api.Controllers
     public class ProductController : ControllerBase
     {
         private readonly ILogger<ProductController> _logger;
+        private readonly IProductService _productService;
 
-        public ProductController(ILogger<ProductController> logger)
+        public ProductController(ILogger<ProductController> logger, IProductService productService)
         {
             _logger = logger;
+            _productService = productService;
         }
 
         [HttpGet]
-        public ActionResult GetAll()
+        public ActionResult GetAllProducts()
         {
+            _logger.LogInformation("Getting all products");
+
+            _productService.GetAllProducts();
             return Ok();
+        }
+
+        [HttpGet("id")]
+        public ActionResult GetProduct()
+        {
+            _logger.LogInformation("Getting a single product with id of: ${id}");
+
+            return Ok();
+
         }
     }
 }
