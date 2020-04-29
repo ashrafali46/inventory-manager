7eb29ca8a390873b2dbf9edd5c08efba642c530c
johnmcraig <jmcraig32884@gmail.com>
2020-04-28 22:18:46 -0400

    Extended service methods that implemented interface

--- src/Services/Product/ProductService.cs
+++ src/Services/Product/ProductService.cs
@@ -1,27 +1,95 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Data;
+using Data.Models;
 
 namespace Services.Product
 {
     public class ProductService : IProductService
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ProductService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public List<Data.Models.Product> GetAllProducts()
         {
-            throw new System.NotImplementedException();
+            return _dbContext.Products.ToList();
         }
 
         public Data.Models.Product GetProductById(int id)
         {
-            throw new System.NotImplementedException();
+            return _dbContext.Products.Find(id);
         }
 
-        public ServiceResponse<bool> CreateProduct(Data.Models.Product product)
+        public ServiceResponse<Data.Models.Product> CreateProduct(Data.Models.Product product)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                _dbContext.Products.Add(product);
+
+                var newInventory = new ProductInventory
+                {
+                    Product = product,
+                    QuantityOnHand = 0,
+                    IdealQuantity = 10
+                };
+
+                _dbContext.ProductInventories.Add(newInventory);
+
+                _dbContext.SaveChanges();
+
+                return new ServiceResponse<Data.Models.Product>
+                {
+                    Data = product,
+                    Time = DateTime.UtcNow,
+                    Message = "Saved new PRoduct",
+                    IsSuccess = true
+                };
+
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResponse<Data.Models.Product>
+                {
+                    Data = product,
+                    Time = DateTime.UtcNow,
+                    Message = $"Could not save new Product, see the following: {ex}",
+                    IsSuccess = false
+                };
+            }
+
         }
 
-        public ServiceResponse<bool> ArchiveProduct(int id)
+        public ServiceResponse<Data.Models.Product> ArchiveProduct(int id)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                var product = _dbContext.Products.Find(id);
+                product.IsArchived = true;
+                _dbContext.SaveChanges();
+
+                return new ServiceResponse<Data.Models.Product>
+                {
+                    Data = product,
+                    Time = DateTime.UtcNow,
+                    Message = "Archived product",
+                    IsSuccess = true
+
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResponse<Data.Models.Product>
+                {
+                    Data = null,
+                    Time = DateTime.UtcNow,
+                    Message = ex.StackTrace,
+                    IsSuccess = false
+                };
+            }
         }
     }
 }
