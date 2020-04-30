f5f1cacc0997c13ed2630b6c22edf76f66756c10
johnmcraig <jmcraig32884@gmail.com>
2020-04-29 23:01:43 -0400

    Brought in VM namespace, mapped properties to Profile and VM

--- src/Api/Profiles/ProductMapper.cs
+++ src/Api/Profiles/ProductMapper.cs
@@ -1,7 +1,23 @@
-﻿namespace Api.Profiles
+﻿
+using Api.ViewModels;
+
+namespace Api.Profiles
 {
     public class ProductMapper
     {
-        
+        public static ProductModel SerializedProductModel(Data.Models.Product product)
+        {
+            return new ProductModel
+            {
+                Id = product.Id,
+                CreatedOn = product.CreatedOn,
+                UpdatedOn = product.UpdatedOn,
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price,
+                IsTaxable = product.IsTaxable,
+                IsArchived = product.IsArchived
+            };
+        }
     }
 }
