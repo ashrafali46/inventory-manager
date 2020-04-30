ffb7bd9e4b34f754cce2d361cfb19b81719b3d67
johnmcraig <jmcraig32884@gmail.com>
2020-04-29 23:15:37 -0400

    Added reverse mapping method

--- src/Api/Profiles/ProductMapper.cs
+++ src/Api/Profiles/ProductMapper.cs
@@ -14,13 +14,33 @@ namespace Api.Profiles
             return new ProductModel
             {
                 Id = product.Id,
-                CreatedOn = product.CreatedOn,
-                UpdatedOn = product.UpdatedOn,
-                Name = product.Name,
-                Description = product.Description,
-                Price = product.Price,
-                IsTaxable = product.IsTaxable,
-                IsArchived = product.IsArchived
+                    CreatedOn = product.CreatedOn,
+                    UpdatedOn = product.UpdatedOn,
+                    Name = product.Name,
+                    Description = product.Description,
+                    Price = product.Price,
+                    IsTaxable = product.IsTaxable,
+                    IsArchived = product.IsArchived
+            };
+        }
+
+        ///<summary>
+        /// Maps a Product VM to a Product data model reverse map
+        ///</summary>
+        ///<params name="product"></params>
+        ///<returns></returns>
+        public static Data.Models.Product SerializedProductModel(ProductModel product)
+        {
+            return new Data.Models.Product
+            {
+                Id = product.Id,
+                    CreatedOn = product.CreatedOn,
+                    UpdatedOn = product.UpdatedOn,
+                    Name = product.Name,
+                    Description = product.Description,
+                    Price = product.Price,
+                    IsTaxable = product.IsTaxable,
+                    IsArchived = product.IsArchived
             };
         }
     }
