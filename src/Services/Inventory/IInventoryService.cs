2cda78a1e708e99712faad3bd82db4acc3764b87
johnmcraig <jmcraig32884@gmail.com>
2020-05-01 00:10:27 -0400

    Created Inventory abstraction in new directory

---
+++ src/Services/Inventory/IInventoryService.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace Services.Inventory
+{
+    public interface IInventoryService
+    {
+         List<Data.Models.ProductInventory> GetCurrentInventory();
+         ServiceResponse<Data.Models.ProductInventory> UpdateUnitsAvailable(int id, int adjusted);
+         Data.Models.ProductInventory GetProductId(int productId);
+         void CreateSnapshot();
+         List<Data.Models.ProductInventorySnapshot> GetSnaphotHistory();
+         
+    }
+}
