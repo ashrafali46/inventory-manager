d670c4808d50f96ebe2663f35172a847e1b62918
johnmcraig <jmcraig32884@gmail.com>
2020-05-03 23:01:29 -0400

    Refactored abstraction and added two response methods

--- src/Services/Order/IOrderService.cs
+++ src/Services/Order/IOrderService.cs
@@ -1,9 +1,12 @@
 using System.Collections.Generic;
+using Data.Models;
 
 namespace Services.Order
 {
     public interface IOrderService
     {
-       List<Data.Models.SalesOrder> GetAllOrders(); 
+       List<SalesOrder> GetAOrders();
+       ServiceResponse<bool> GenerateInvoiceForOrder(SalesOrder order);
+       ServiceResponse<bool> MarkComplete(int id); //mark fulfilled
     }
 }
