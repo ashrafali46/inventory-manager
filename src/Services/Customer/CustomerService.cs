5ebfed2674c4693e0e1edaae9a9148cd97221685
johnmcraig <jmcraig32884@gmail.com>
2020-05-05 00:30:46 -0400

    Created methods with exception handling for deleting, adding, and find by id

--- src/Services/Customer/CustomerService.cs
+++ src/Services/Customer/CustomerService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Data;
@@ -16,25 +17,82 @@ namespace Services.Customer
 
         public ServiceResponse<Data.Models.Customer> CreateCustomer(Data.Models.Customer customer)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                _dbContext.Customers.Add(customer);
+
+                _dbContext.SaveChanges();
+
+                return new ServiceResponse<Data.Models.Customer>
+                {
+                    IsSuccess = true,
+                    Message = "New customer added",
+                    Time = DateTime.UtcNow,
+                    Data = customer
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResponse<Data.Models.Customer>
+                {
+                    IsSuccess = false,
+                    Message = $"Failed to add a new customer with the following error: {ex}",
+                    Time = DateTime.UtcNow,
+                    Data = customer
+                };
+            }
         }
 
         public ServiceResponse<bool> DeleteCustomer(int id)
         {
-            throw new System.NotImplementedException();
+            var customer = _dbContext.Customers.Find(id);
+
+            if (customer == null)
+                return new ServiceResponse<bool>
+                {
+                    Time = DateTime.UtcNow,
+                    IsSuccess = false,
+                    Message = $"the Customer object by {id} could not be found",
+                    Data = false
+                };
+
+            try
+            {
+                _dbContext.Customers.Remove(customer);
+
+                _dbContext.SaveChanges();
+
+                return new ServiceResponse<bool>
+                {
+                    IsSuccess = true,
+                    Message = $"The customer object with {id} was removed from the database",
+                    Time = DateTime.UtcNow,
+                    Data = false
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResponse<bool>
+                {
+                    IsSuccess = false,
+                    Message = $"Failed to add a new customer with the following error: {ex}",
+                    Time = DateTime.UtcNow,
+                    Data = false
+                };
+            }
         }
 
         public List<Data.Models.Customer> GetAllCustomers()
         {
             return _dbContext.Customers
-                    .Include(x => x.PrimaryAddress)
-                    .OrderBy(x => x.LastName)
-                    .ToList();
+                .Include(x => x.PrimaryAddress)
+                .OrderBy(x => x.LastName)
+                .ToList();
         }
 
         public Data.Models.Customer GetById(int id)
         {
-            throw new System.NotImplementedException();
+            return _dbContext.Customers.Find(id);
         }
     }
 }
