9f57220397b211bc5333dea2ba76fab28020e72c
johnmcraig <jmcraig32884@gmail.com>
2020-05-05 23:52:56 -0400

    Syntax cleanup

--- src/Services/Product/ProductService.cs
+++ src/Services/Product/ProductService.cs
@@ -14,6 +14,7 @@ namespace Services.Product
         {
             _dbContext = dbContext;
         }
+        
         public List<Data.Models.Product> GetAllProducts()
         {
             return _dbContext.Products.ToList();
