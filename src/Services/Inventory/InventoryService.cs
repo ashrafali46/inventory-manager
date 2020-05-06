796b638c5c9f90e23bbd0a546cdb6c08e6156b83
johnmcraig <jmcraig32884@gmail.com>
2020-05-05 23:52:43 -0400

    Injected context to be readt

--- src/Services/Inventory/InventoryService.cs
+++ src/Services/Inventory/InventoryService.cs
@@ -1,10 +1,18 @@
 using System.Collections.Generic;
+using Data;
 using Data.Models;
 
 namespace Services.Inventory
 {
     public class InventoryService : IInventoryService
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public InventoryService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public void CreateSnapshot()
         {
             throw new System.NotImplementedException();
