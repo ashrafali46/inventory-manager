70a7bf0978e5224bbb1aa722fadf414cb8592f48
johnmcraig <jmcraig32884@gmail.com>
2020-05-08 23:48:13 -0400

    Implemented method to update inventory with error handling

--- src/Services/Inventory/InventoryService.cs
+++ src/Services/Inventory/InventoryService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Data;
@@ -40,7 +41,34 @@ namespace Services.Inventory
 
         public ServiceResponse<ProductInventory> UpdateUnitsAvailable(int id, int adjusted)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                var inventory = _dbContext.ProductInventories
+                    .Include(inv => inv.Product)
+                    .First(inv => inv.Product.Id == id);
+
+                inventory.QuantityOnHand += adjusted;
+
+                _dbContext.SaveChanges();
+
+                return new ServiceResponse<ProductInventory> 
+                {
+                    IsSuccess = true,
+                    Data = inventory,
+                    Message = $"Product with {id} had its inventory adjusted",
+                    Time = DateTime.UtcNow
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResponse<ProductInventory> 
+                {
+                    IsSuccess = false,
+                    Data = null,
+                    Message = $"Error updating Product Inventory and Quantity On Hand, see the following error: {ex}",
+                    Time = DateTime.UtcNow
+                };
+            }
         }
     }
 }
