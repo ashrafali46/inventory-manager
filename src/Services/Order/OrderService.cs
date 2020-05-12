f79585f73b6fece270b15753a66ae86947e018c3
johnmcraig <jmcraig32884@gmail.com>
2020-05-11 23:29:38 -0400

    Injected context and logger, implemented method to get all orders

--- src/Services/Order/OrderService.cs
+++ src/Services/Order/OrderService.cs
@@ -1,10 +1,23 @@
 using System.Collections.Generic;
+using System.Linq;
+using Data;
 using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Services.Order
 {
     public class OrderService : IOrderService
     {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly ILogger<OrderService> _logger;
+
+        public OrderService(ApplicationDbContext dbContext, ILogger<OrderService> logger)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+        }
+
         public ServiceResponse<bool> GenerateInvoiceForOrder(SalesOrder order)
         {
             throw new System.NotImplementedException();
@@ -12,7 +25,10 @@ namespace Services.Order
 
         public List<SalesOrder> GetAllOrders()
         {
-            throw new System.NotImplementedException();
+            return _dbContext.SalesOrders
+                .Include(so => so.Customer)
+                .Include(so => so.SalesOrderItems)
+                .ToList();
         }
 
         public ServiceResponse<bool> MarkComplete(int id)
