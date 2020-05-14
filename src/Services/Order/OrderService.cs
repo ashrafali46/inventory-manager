c089eb7a4173f94779687330228b8e1a1fed7f13
johnmcraig <jmcraig32884@gmail.com>
2020-05-13 23:49:37 -0400

    Implemented Invoice creation method

--- src/Services/Order/OrderService.cs
+++ src/Services/Order/OrderService.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Data;
 using Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Services.Inventory;
+using Services.Product;
 
 namespace Services.Order
 {
@@ -11,23 +14,61 @@ namespace Services.Order
     {
         private readonly ApplicationDbContext _dbContext;
         private readonly ILogger<OrderService> _logger;
+        private readonly IProductService _productService;
+        private readonly IInventoryService _inventoryService;
 
-        public OrderService(ApplicationDbContext dbContext, ILogger<OrderService> logger)
+        public OrderService(ApplicationDbContext dbContext,
+            ILogger<OrderService> logger, IProductService productService, IInventoryService inventoryService)
         {
             _dbContext = dbContext;
             _logger = logger;
+            _productService = productService;
+            _inventoryService = inventoryService;
         }
 
         public ServiceResponse<bool> GenerateInvoiceForOrder(SalesOrder order)
         {
-            throw new System.NotImplementedException();
+            foreach (var item in order.SalesOrderItems)
+            {
+                item.Product = _productService.GetProductById(item.Product.Id);
+
+                var inventoryId = _inventoryService.GetProductId(item.Product.Id).Id;
+
+                _inventoryService.UpdateUnitsAvailable(inventoryId, -item.Quantity);
+            }
+
+            try
+            {
+                _dbContext.SalesOrders.Add(order);
+                _dbContext.SaveChanges();
+
+                return new ServiceResponse<bool>
+                {
+                    IsSuccess = true,
+                    Data = true,
+                    Message = "An order was created",
+                    Time = DateTime.UtcNow
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResponse<bool>
+                {
+                    IsSuccess = false,
+                    Data = false,
+                    Message = $"An error occurred with the following: {ex}",
+                    Time = DateTime.UtcNow
+                };
+            }
         }
 
         public List<SalesOrder> GetAllOrders()
         {
             return _dbContext.SalesOrders
                 .Include(so => so.Customer)
+                .ThenInclude(customer => customer.PrimaryAddress)
                 .Include(so => so.SalesOrderItems)
+                .ThenInclude(item => item.Product)
                 .ToList();
         }
 
