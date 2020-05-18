3d28c7b81a5e0692f92c267d0d53222fbeb598a0
johnmcraig <jmcraig32884@gmail.com>
2020-05-17 23:34:37 -0400

    Renamed method to be more specific

--- src/Services/Order/IOrderService.cs
+++ src/Services/Order/IOrderService.cs
@@ -6,7 +6,7 @@ namespace Services.Order
     public interface IOrderService
     {
        List<SalesOrder> GetAllOrders();
-       ServiceResponse<bool> GenerateInvoiceForOrder(SalesOrder order);
+       ServiceResponse<bool> GenerateOpenOrder(SalesOrder order);
        ServiceResponse<bool> MarkComplete(int id); //mark fulfilled
     }
 }
