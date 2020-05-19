61b1a9455f8baaa5ad300648783c77190bdda73e
johnmcraig <jmcraig32884@gmail.com>
2020-05-18 23:30:24 -0400

    Created order controller, setup abstractions to be read

---
+++ src/Api/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+using Api.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Services.Customer;
+using Services.Order;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly ILogger<OrderController> _logger;
+        private readonly IOrderService _orderService;
+        private readonly ICustomerService _customerService;
+
+        public OrderController(ILogger<OrderController> logger,IOrderService orderService, ICustomerService customerService)
+        {
+            _customerService = customerService;
+            _orderService = orderService;
+            _logger = logger;
+        }
+
+        public ActionResult GenerateNewOrder(InvoiceModel invoice)
+        {
+            return Ok();
+        }
+    }
+}
