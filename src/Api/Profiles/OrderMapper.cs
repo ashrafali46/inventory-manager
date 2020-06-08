252f8eec20952d52be70ca19cf33bc6486dde040
johnmcraig <jmcraig32884@gmail.com>
2020-06-08 00:42:43 -0400

    Refactored OrderMapper class

--- src/Api/Profiles/OrderMapper.cs
+++ src/Api/Profiles/OrderMapper.cs
@@ -1,25 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Api.ViewModels;
+using Data.Models;
 
 namespace Api.Profiles
 {
     public class OrderMapper
     {
-        public static OrderModel SerializedInvoiceOrder(Data.Models.SalesOrder order)
+        public static InvoiceModel SerializedInvoiceOrder(SalesOrder invoice)
         {
-            return new OrderModel
+            return new InvoiceModel
             {
 
             };
 
         }
 
-        public static Data.Models.SalesOrder OrderSerializedModel(OrderModel order)
+        public static SalesOrder SerializedInvoiceOrder(InvoiceModel invoice)
         {
-            return new Data.Models.SalesOrder
+            var salesOrderItems = invoice.LineItems.Select(item => new SalesOrderItem
             {
-                Id = order.Id,
+                Id = item.Id,
+                    Quantity = item.Quantity,
+                    Product = ProductMapper.SerializedProductModel(item.Product)
+            }).ToList();
 
+            return new SalesOrder
+            {
+                SalesOrderItems = salesOrderItems,
+                    UpdatedOn = DateTime.UtcNow,
+                    CreatedOn = DateTime.UtcNow
             };
+        }
+
+        public static List<OrderModel> SerializeOrderToViewModel()
+        {
+            throw new Exception();
         }
     }
 }
