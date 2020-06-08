67709eb82c21b7add3ee9bc51534dcbf87fa58ef
johnmcraig <jmcraig32884@gmail.com>
2020-06-08 00:41:23 -0400

    Refactored signature to ViewModel property

--- src/Api/ViewModels/OrderModel.cs
+++ src/Api/ViewModels/OrderModel.cs
@@ -9,7 +9,7 @@ namespace Api.ViewModels
         public int Id { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime UpdatedOn { get; set; }
-        public Customer Customer { get; set; }
+        public CustomerModel Customer { get; set; }
         public List<SalesOrderItem> SalesOrderItems { get; set; }
         public bool IsPaid { get; set; }
     }
