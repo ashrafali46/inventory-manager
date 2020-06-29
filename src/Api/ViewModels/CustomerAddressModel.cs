e19621e9e9c1c65a7034bf019718f7fa71996582
johnmcraig <jmcraig32884@gmail.com>
2020-06-28 22:00:07 -0400

    Added properties matching CustomerAddress entity

--- src/Api/ViewModels/CustomerAddressModel.cs
+++ src/Api/ViewModels/CustomerAddressModel.cs
@@ -1,6 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Api.ViewModels
 {
     public class CustomerAddressModel
     {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+
+        [MaxLength(100)]
+        public string AddressLine1 { get; set; }
+
+        [MaxLength(100)]
+        public string AddressLine2 { get; set; }
+
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
     }
 }
