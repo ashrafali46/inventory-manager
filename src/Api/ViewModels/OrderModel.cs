5a2da33e51d2930d3b26201d0d6b807f5b45a203
johnmcraig <jmcraig32884@gmail.com>
2020-07-18 00:16:34 -0400

    Refactored property in Order view model to sales order item view model

--- src/Api/ViewModels/OrderModel.cs
+++ src/Api/ViewModels/OrderModel.cs
@@ -10,7 +10,7 @@ namespace Api.ViewModels
         public DateTime CreatedOn { get; set; }
         public DateTime UpdatedOn { get; set; }
         public CustomerModel Customer { get; set; }
-        public List<SalesOrderItem> SalesOrderItems { get; set; }
+        public List<SalesOrderItemModel> SalesOrderItems { get; set; }
         public bool IsPaid { get; set; }
     }
 }
